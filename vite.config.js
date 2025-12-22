1ec9c10d98ade1c5f99549002278c91235a4a838
babu-admin <shimz777@gmail.com>
2025-12-21 19:43:32 -0500

    サブドメイン運用へ変更

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wordrobe/', 
+  base: '/',  // ★ここを '/wordrobe/' から '/' に変更！
   build: {
     outDir: 'dist',
   },
