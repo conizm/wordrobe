4d50b04d0622065e1c4eb308e04e482c377ba49c
babu-admin <shimz777@gmail.com>
2025-12-21 19:16:17 -0500

    Vite版へ完全移行

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+// public/sw.js
+
+const CACHE_NAME = 'wordrobe-v3-new'; 
+const urlsToCache = [
+  '/wordrobe/',
+  '/wordrobe/index.html',
+  '/wordrobe/manifest.webmanifest',
+  '/wordrobe/images/wordrobe-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
